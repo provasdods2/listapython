d509404cfb88c9c00dc4c5f297575a7b9e8d36d5
provasdods2 <provasdosds2@gmail.com>
2026-04-29 14:06:45 +0000

    v.1

--- sentinela.py
+++ sentinela.py
@@ -1,1 +1,6 @@
-
+#--Variável Sentinela--
+while True:
+    comando =  input("Digite o Comando. Se precisar parar digite:Sair")
+    if comando == "sair":
+        break
+    print(f"Executando[comando]")
