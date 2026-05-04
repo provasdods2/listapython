86facd78316d2163f70fd877793fb09dfb24d082
provasdods2 <provasdosds2@gmail.com>
2026-05-04 07:51:53 -0300

    Update seletor.py

--- seletor.py
+++ seletor.py
@@ -1,1 +1,8 @@
-
+#seletor de caracteres--
+letra = input("Diggte uma letra").upper()
+letra == 'A':
+ print("Você Selecionou a opção A")
+elif letra == 'B':
+ print("Você selecionou a opção B")
+else:
+ print("Essa opção não está disponível")
